c4cb4c48b9a29b254c52749a415ed8b82c7037c1
lanarosedeguzman@gmail.com <lanarosedeguzman@gmail.com>
2025-03-27 20:32:24 +0800

    Initial push

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,7 @@
+import Name from './Name.jsx';
+
+function App() {
+  <Name/>
+}
+
+export default App
---
+++ portfolio/src/Name.jsx
@@ -0,0 +1,9 @@
+function Name() {
+    return(
+        <div class="nameContainer">
+            <p class="nameTitle">Lana Rose</p>
+        </div>
+    )
+}
+
+export default Name
